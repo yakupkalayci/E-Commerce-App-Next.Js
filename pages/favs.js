1f23cd483c21608625eb6da62e15086edc94e512
yakupkalayci <ykpklyc18@gmail.com>
2022-05-26 22:53:28 +0300

    desing favs page

--- pages/favs.js
+++ pages/favs.js
@@ -1,14 +1,18 @@
 import { useProduct } from "../context/ProductsContext";
 import styles from "../styles/favs.module.css";
+import { Heading, Button } from "@chakra-ui/react";
+import Link from "next/link";
 
 export default function Favs() {
   const {state, dispatch} = useProduct();
 
   return (
     <div className={styles.favs}>
-      <h2>Favourites</h2>
+      <Heading size="lg">Favourites</Heading>
       <ul>
-        {state.favs.map((item) => (
+        {
+        (state.favs.length > 0) ?
+        state.favs.map((item) => (
           <li className={styles.product} key={item.title}>
             <div>
               <img src={item.img} width="200px" height="200px"></img>
@@ -16,11 +20,20 @@ export default function Favs() {
             <div>
               {item.title}
             </div>
-            <div>
-              <button onClick={() => dispatch({type:"RemoveFromFavs", id:item.id})} >Remove</button>
+            <div className={styles.buttons}>
+              <Button onClick={() => dispatch({type:"AddToCart", id:item.id})} colorScheme="pink" mb="2" >Add to Cart</Button>
+              <Button onClick={() => dispatch({type:"RemoveFromFavs", id:item.id})} colorScheme="blue" >Remove</Button>
             </div>
           </li>
-        ))}
+        ))
+        :<div className={styles.infoContainer}>
+          <p className={styles.warningMessage}>You haven't added any products to your favorites yet.</p>
+          <br />
+          <Link href="/">
+          <a className={styles.link}>See All Products</a>
+          </Link>
+        </div>
+      }
 
       </ul>
     </div>
