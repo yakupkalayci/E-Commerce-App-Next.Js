e8eeca8a045a64fb1478fd28c659bfcf7a8faa04
yakupkalayci <ykpklyc18@gmail.com>
2022-09-30 22:31:00 +0300

    make responsive to app

--- components/Header.js
+++ components/Header.js
@@ -10,51 +10,62 @@ export default function Header() {
 
   return (
     <header className={styles.headerContainer}>
+
       <div className={styles.mainHeader}>
-        <div id="logo">
+        
+        <div id="logo" className={styles.logo}>
           <Link href="/">
             <a>
               <Heading>E-Commerce APP</Heading>
             </a>
           </Link>
         </div>
-        <div className={styles.search}>
-          <Input
-            placeholder="Type for search"
-            variant="Outline"
-            color="black"
-            width="300px"
-            height="auto"
-            onChange={(e) => handleInputChange(e)}
-          />
-          <IconButton icon={<FiSearch />} color="black" size="md" />
-        </div>
-        <div className={styles.buttonGroup}>
-          <Link href="/favs">
-            <IconButton
-              aria-label="Favs button"
-              colorScheme="blue"
-              icon={<FiHeart />}
-              size="lg"
-              className={styles.btn}
+
+        <div className={styles.headerRight}>
+          <div className={styles.search}>
+            <Input
+              placeholder="Type for search"
+              variant="Outline"
+              color="black"
+              width="300px"
+              height="auto"
+              onChange={(e) => handleInputChange(e)}
             />
-          </Link>
-          <Link href="/cart">
-            <IconButton
-              aria-label="Cart button"
-              colorScheme="pink"
-              icon={<FiShoppingCart />}
-              size="lg"
-              className={styles.btn}
-            />
-          </Link>
+            <IconButton icon={<FiSearch />} color="black" size="md" />
+          </div>
+
+          <div className={styles.buttonGroup}>
+            <Link href="/favs">
+              <IconButton
+                aria-label="Favs button"
+                colorScheme="blue"
+                icon={<FiHeart />}
+                size="lg"
+                className={styles.btn}
+              />
+            </Link>
+            <Link href="/cart">
+              <IconButton
+                aria-label="Cart button"
+                colorScheme="pink"
+                icon={<FiShoppingCart />}
+                size="lg"
+                className={styles.btn}
+              />
+            </Link>
+          </div>
         </div>
+
+
+
       </div>
+
       <div className={styles.subHeader}>
         <div className={styles.subHeaderContainer}>
           <Navbar />
         </div>
       </div>
+
     </header>
   );
 }
